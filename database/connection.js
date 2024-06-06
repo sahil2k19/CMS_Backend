9becf468a49697af3ceaca8c4073ff9699456afb
Sahil Gagan <sahilgagan227@gmail.com>
2024-06-06 23:42:54 +0530

    new commit

--- database/connection.js
+++ database/connection.js
@@ -4,7 +4,8 @@ const { DBNAME, DBHOST,DBUSER, DBPASS, DBPORT } = process.env;
 const sequelize = new Sequelize(DBNAME, DBUSER, DBPASS, {
     host: DBHOST,
     port: DBPORT,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectModule: require('mysql2'),
 });
 
 module.exports = sequelize;
