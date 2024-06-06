a18dd86737cc45ef06bf26b0bdedf16a1bf2954b
Sahil Gagan <sahilgagan227@gmail.com>
2024-06-06 23:33:35 +0530

    resolve

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,6 @@
 const {Sequelize} = require('sequelize');
 
 const { DBNAME, DBHOST,DBUSER, DBPASS, DBPORT } = process.env;
-console.log(DBNAME, DBHOST,DBUSER, DBPASS, DBPORT)
 const sequelize = new Sequelize(DBNAME, DBUSER, DBPASS, {
     host: DBHOST,
     port: DBPORT,
